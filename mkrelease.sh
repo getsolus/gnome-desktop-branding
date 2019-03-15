f9187d9bc36345cae947f3dbac3b6a134a2dc129
Joshua Strobl <joshua@stroblindustries.com>
2019-03-15 20:19:17 +0200

    Release v15

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,10 +4,9 @@ set -e
 git submodule init
 git submodule update
 
-# Script for ikey because he went with meson. *shrug*
-VERSION="14"
+VERSION="15"
 NAME="gnome-desktop-branding"
-git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+git-archive-all --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
 
 gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
