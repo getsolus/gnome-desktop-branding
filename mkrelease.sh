9994dfa954692dd0237bee8781bc7a403f4f54e6
Joey Riches <josephriches@gmail.com>
2023-06-19 15:54:49 +0100

    Bump version to v17

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,10 +4,10 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="15"
+VERSION="17"
 NAME="gnome-desktop-branding"
 git-archive-all --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
 
 gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
-gpg --verify "${NAME}-${VERSION}.tar.xz.asc"               
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"
